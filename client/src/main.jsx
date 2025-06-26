11a798dffadd68087a220b2bb8b2cd07ab55ef59
Wes Imac mini 1M <wes@wess-mac-mini.myfiosgateway.com>
2025-06-26 13:22:46 -0400

    Updating navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,17 @@ import Admin from './Admin.jsx';
 import App from './App.jsx';
 import Report from './Report.jsx'; // <-- make sure this exists
 import './index.css';
-import KDS from './KdsComponent.jsx'; 
+import KDS from './KdsComponent.jsx';
+import DailySpecials from './dailySpecials.jsx'; // Fixed import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/report" element={<Report />} />
-      <Route path="/admin" element={<Admin />} />  {/* ✅ Add this */}
-      <Route path="/kds" element={<KDS />} /> 
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/dailySpecials" element={<DailySpecials />} /> // Updated to match import
+      <Route path="/kds" element={<KDS />} />
     </Routes>
   </BrowserRouter>
-)
+);
