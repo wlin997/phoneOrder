f6d2bb08bd1bf9ab7f82f280769b0d4380c0a4d4
Wes Imac mini 1M <wes@wess-mac-mini.myfiosgateway.com>
2025-07-10 23:08:21 -0400

    update AuthContext.jsx

--- client/src/AuthContext.jsx
+++ client/src/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState, useCallback } from "react";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 
 /*********************************************************
