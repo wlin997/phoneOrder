615fa039c397be4931bd8f5e5bbe8f3e4fb3862e
Wes Imac mini 1M <wes@wess-mac-mini.myfiosgateway.com>
2025-07-25 17:01:57 -0400

    update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -6,7 +6,7 @@ process.env.TZ = 'America/New_York'; // Set default timezone
 const express = require("express");
 const adminRoutes = require("./admin.routes.cjs");
 const authRoutes = require("./auth.routes.cjs"); // Import the new auth routes
-const { authenticateToken, authorizePermissions } = require("./auth.middleware.cjs");
+const { authenticateToken, authorizePermission } = require("./auth.middleware.cjs");
 
 const fs = require("fs");
 const fsp = require("fs").promises;
